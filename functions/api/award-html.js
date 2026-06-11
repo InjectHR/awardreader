1ace6c1bfa2661bf1b21c3db81265827c6f32ec6
InjectHR <chloes@inject.com.au>
2026-06-11 10:12:19 +1000

    Update award-html.js

--- functions/api/award-html.js
+++ functions/api/award-html.js
@@ -1,1 +1,18 @@
+export async function onRequestGet(context) {
+  const url = new URL(context.request.url);
+  const code = (url.searchParams.get("code") || "").toUpperCase();
 
+  if (!/^MA\d{6}$/.test(code)) {
+    return new Response("Invalid award code", { status: 400 });
+  }
+
+  const awardUrl = `https://awards.fairwork.gov.au/${code}.html`;
+  const response = await fetch(awardUrl);
+  const html = await response.text();
+
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html; charset=utf-8"
+    }
+  });
+}
